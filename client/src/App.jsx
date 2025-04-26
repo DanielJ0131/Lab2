d2322b066b769f44be286944bb3f81636430036a
DanielJ0131 <daniel.jonsson0131@gmail.com>
2025-04-26 12:00:27 +0200

    Add client

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ProjectAssignmentsTable from './components/ProjectAssignmentsTable'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Lab 2 Full Stack</h1>
+      </header>
+      <ProjectAssignmentsTable />
+      <footer>
+        <p>© Daniel Jönsson</p>
+        <a href="mailto:daniel.jonsson0131@stud.hkr.se">daniel.jonsson0131@stud.hkr.se</a>
+      </footer>
+    </div>
+  )
+}
+
+export default App
